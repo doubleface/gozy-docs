d390b9b7d3429d3588c329d4609ec2e553f05ed7
doubleface <christophe@cozycloud.cc>
2018-03-22 17:54:46 +0100

    Add cozy-stack doc

--- build.sh
+++ build.sh
@@ -19,6 +19,9 @@ cp index.html docs/
 fetch-from-remote https://github.com/cozy/cozy-konnector-libs.git /tmp/cozy-konnector-libs
 cp -r /tmp/cozy-konnector-libs/packages/cozy-konnector-libs/docs src/cozy-konnector-libs
 
+fetch-from-remote https://github.com/cozy/cozy-stack.git /tmp/cozy-stack
+cp -r /tmp/cozy-stack/docs src/cozy-stack
+
 mkdocs build -f mkdocs.yml
 mkdocs build -f mkdocs_fr.yml
 msgmerge --update i18n/fr_FR/LC_MESSAGES/messages.po i18n/messages.pot
