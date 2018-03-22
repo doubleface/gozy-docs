9a699bb3f9fb424b0f4510d77af9f72672461692
Patrick Browne <pt.browne@gmail.com>
2018-03-22 14:10:39 +0100

    feat: add cozy-konnector-libs

--- build.sh
+++ build.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
+function fetch-from-remote {
+    repo=$1
+    dir=$2
+    mkdir -p $2
+    cd $dir
+    if [[ -e .git ]]; then
+        git clone $repo --depth 1
+    else
+        git pull
+    fi
+    cd -
+}
+
 rm -rf docs/*
 cp index.html docs/
+
+fetch-from-remote https://github.com/cozy/cozy-konnector-libs.git /tmp/cozy-konnector-libs
+cp -r /tmp/cozy-konnector-libs/packages/cozy-konnector-libs/docs src/cozy-konnector-libs
+
 mkdocs build -f mkdocs.yml
 mkdocs build -f mkdocs_fr.yml
 msgmerge --update i18n/fr_FR/LC_MESSAGES/messages.po i18n/messages.pot
